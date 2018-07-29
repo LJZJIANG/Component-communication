f391cf2d51f135957c3182bfce8a3694a12bf8ca
LJZJIANG <1457921091@qq.com>
2018-07-29 09:43:36 +0800

    code upload

---
+++ src/components/page1.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <p><span>单价：</span><span>{{price}}</span> <button @click="downPrice">降价1元</button></p>
+        <p>数量: {{count}} </p>
+        
+    </div>
+</template>
+<script>
+import bus from  '../eventBus.js'
+export default {
+    props:{
+        price:{
+            type:String,
+            default:''
+        }
+    },
+    data(){
+        return{
+            count:10
+        }
+    },
+    methods:{
+        downPrice(){
+            this.$emit('downPrice')
+        }
+    },
+    watch:{
+       price(newPrice){
+          bus.$emit('priceChange',newPrice,this.count) 
+       } 
+    }
+}
+</script>
---
+++ src/components/page2.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>
+            <span>总金额：{{totalMoney}}元 </span>剩余金额：
+            <span>{{balance}}元</span>
+        </p>
+    </div>
+</template>
+<script>
+import bus from "../eventBus.js";
+export default {
+  data() {
+    return {
+      balance: 1000,
+      totalMoney: 1000
+    };
+  },
+  mounted() {
+    bus.$on("priceChange", (price, count) => {
+      this.balance = this.totalMoney - price * count;
+    });
+    console.log(this.$parent)
+  }
+};
+</script>
